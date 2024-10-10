f4bd8d3c3a036855bb15bc0f8c9a1eb23b9c6cf7
nitesh-20 <nsahu6156@gmail.com>
2024-10-10 11:36:50 +0530

    GCD of Two Numbers

---
+++ Day11/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int gcd(int a, int b){
+    while(b!=0){
+        int temp=b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+int main(){
+    int num1,num2;
+
+    printf("Enter two integer: ");
+    scanf("%d %d",&num1,&num2);
+    printf("GCD of %d and %d is %d",num1,num2,gcd(num1,num2));
+    return 0;
+}
