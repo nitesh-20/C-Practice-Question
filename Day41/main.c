58f543569362ccb11e7bc7e21ba8e06479044a1c
nitesh-20 <nsahu6156@gmail.com>
2024-11-03 12:32:22 +0530

    1. While Loop or While-Do Loop Questions //  Reverse A given Number

---
+++ Day41/main.c
@@ -0,0 +1,19 @@
+// 1. While Loop or While-Do Loop Questions
+//  Reverse A given Number
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        reversed = reversed * 10 + (num % 10);
+        num = num / 10;
+    }
+
+    printf("Reversed number = %d\n", reversed);
+
+    return 0;
+}
