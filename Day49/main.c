48dcac90410d1cfb6eda9776bef35e34516fee69
nitesh-20 <nsahu6156@gmail.com>
2024-12-30 20:47:45 +0530

    Count the Number of Digits of an Integer

---
+++ Day49/main.c
@@ -0,0 +1,22 @@
+// Count the Number of Digits of an Integer
+#include <stdio.h>
+
+int countDigits(int number) {
+    int count = 0;
+    if (number < 0) {
+        number = -number;
+    }
+    do {
+        count++;
+        number /= 10;
+    } while (number != 0);
+    return count;
+}
+
+int main() {
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    printf("The number of digits in %d is: %d\n", number, countDigits(number));
+    return 0;
+}
