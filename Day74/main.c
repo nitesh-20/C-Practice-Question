2bbb509a053423c766bc7bea0b0b0c890074ff76
nitesh-20 <nsahu6156@gmail.com>
2025-02-10 13:32:06 +0530

    Create a Simple Calculator using a switch cas

---
+++ Day74/main.c
@@ -0,0 +1,37 @@
+// Create a Simple Calculator using a switch case
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("Result: %.2lf\n", num1 / num2);
+            else
+                printf("Error: Division by zero is not allowed.\n");
+            break;
+        default:
+            printf("Invalid operator! Please use +, -, *, or /.\n");
+    }
+
+    return 0;
+}
