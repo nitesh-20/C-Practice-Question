5e38e54f1e707c7d6e9a16d6f03f133912b550ab
nitesh-20 <nsahu6156@gmail.com>
2025-02-22 10:19:08 +0530

    Find a maximum and minimum element in an array-using recursion.

--- Day85/main.c
+++ Day85/main.c
@@ -15,12 +15,11 @@ void findMaxMin(int arr[], int n, int index, int *max, int *min) {
 
     findMaxMin(arr, n, index + 1, max, min);
 }
-
 int main() {
     int arr[] = {12, 5, 87, 3, 22, 45, 78};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    int max = arr[0], min = arr[0]; // Initialize max and min
+    int max = arr[0], min = arr[0]; 
 
     findMaxMin(arr, n, 1, &max, &min);
 
