594db802aac0b4531ee5d46b7b59d963fdf9e5b3
nitesh-20 <nsahu6156@gmail.com>
2025-03-04 13:28:40 +0530

    c++

---
+++ Day92/main.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "Hello World!" << endl;
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Sum: " << a+b << endl;
+    return 0;
+}
