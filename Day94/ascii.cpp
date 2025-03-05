078fe67a5ed5654ce00e84e623ef8a1841d16540
nitesh-20 <nsahu6156@gmail.com>
2025-03-05 14:52:55 +0530

    Ascii value of the character

---
+++ Day94/ascii.cpp
@@ -0,0 +1,12 @@
+// Print Ascii Value of the Character
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c;
+    cout << "Enter a character: ";
+    cin >> c;
+
+    cout << "Acii value of " << c << " is " << int(c) << endl;
+    return 0;
+}
