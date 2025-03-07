31dac2dd7ef36cd9b6e4074da96efb6277ad27ef
nitesh-20 <nsahu6156@gmail.com>
2025-03-07 14:44:14 +0530

    program that takes user input and prints a message:

---
+++ Day95/main.cpp
@@ -0,0 +1,11 @@
+// program that takes user input and prints a message:
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << "! Welcome to C++ programming." << endl;
+    return 0;
+}
