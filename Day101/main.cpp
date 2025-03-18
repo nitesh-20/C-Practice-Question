5ba6501bb68442ece56a24ac43bd1e7d9fdbd09a
nitesh-20 <nsahu6156@gmail.com>
2025-03-18 14:42:59 +0530

    hod details program

--- Day101/main.cpp
+++ Day101/main.cpp
@@ -1,45 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-struct College
-{
-    string branch;
-};
-struct HodDetail{
+
+struct Branch {
     string name;
-    string branch;
+};
+
+struct HodDetails {
+    string name;
     int age;
     float salary;
 };
-int main(){
-        HodDetail h1;
-        h1.name="Rahul";
-        h1.branch="CSE";
-        h1.age=35;
-        h1.salary=600;
 
-        HodDetail h2;
-        h2.name="Sourabh";
-        h2.branch="ECE";
-        h2.age=30;
-        h2.salary=550;
+int main() {
+    int n = 3;
+    Branch* branches = new Branch[n];
+    HodDetails* hods = new HodDetails[n];
 
+    string branchNames[] = {"CSE", "ME", "EE"};
+    string hodNames[] = {"Raj", "Rahul", "Ravi"};
+    int ages[] = {30, 35, 40};
+    float salaries[] = {30000, 35000, 40000};
 
-        HodDetail h3;
-        h3.name="Kapil";
-        h3.branch="ME";
-        h3.age=40;
-        h3.salary=650;
+    string branchName;
+    cout << "Enter branch name: ";
+    cin >> branchName;
 
-        HodDetail h4;
-        h4.name="Nitesh";
-        h4.branch="AIML";
-        h4.age=38;
-        h4.salary=620;
+    for (int i = 0; i < n; i++) {
+        branches[i].name = branchNames[i];
+        hods[i].name = hodNames[i];
+        hods[i].age = ages[i];
+        hods[i].salary = salaries[i];
+    }
 
-        cout<<"HOD Details"<<endl;
-        cout<<"Name\tBranch\tAge\tsalary"<<endl;
-
-        cout<<h1.name<<"\t"<<h1.branch<<"\t"<<h1.age<<"\t"<<h1.salary<<endl<<h2.name<<"\t"<<h2.branch<<"\t"<<h2.age<<"\t"<<h2.salary<<endl<<h3.name<<"\t"<<h3.branch<<"\t"<<h3.age<<"\t"<<h3.salary<<endl<<h4.name<<"\t"<<h4.branch<<"\t"<<h4.age<<"\t"<<h4.salary<<endl;
-
-        return 0;
+    for (int i = 0; i < n; i++) {
+        if (branches[i].name == branchName) {
+            cout << "Branch: " << branches[i].name << endl;
+            cout << "HOD Name: " << hods[i].name << endl;
+            cout << "HOD Age: " << hods[i].age << endl;
+            cout << "HOD Salary: " << hods[i].salary << endl;
+        }
+    }
+    return 0;
 }
