e559c643193c0384199a636c8c59118ffdc1b2e0
nitesh-20 <nsahu6156@gmail.com>
2025-04-01 22:15:36 +0530

    DAY114

---
+++ Day114/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=10;
+    int mul=5;
+    for (int  i = 1; i <= 10;   i++)
+    {
+        cout <<mul<<"x"<<i<<"="<< mul*i << endl;
+    }
+    return 0;
+}
