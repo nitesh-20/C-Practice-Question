a364e8b74ed0cfe44783e0a9bae26ca599d03194
nitesh-20 <nsahu6156@gmail.com>
2025-04-19 08:43:24 +0530

    Day123 Leaner search

--- Day123/main.cpp
+++ Day123/main.cpp
@@ -1,25 +1,17 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    int arr[5] = {10, 20, 30, 40, 50};
+int main(){
+    int a[5]={1,2,33,42,53};
     int key;
-    bool found = false;
 
-    cout << "Enter number to search: ";
-    cin >> key;
+    cout<<"Enter the key to search: ";
+    cin>>key;
 
-    for (int i = 0; i < 5; i++) {
-        if (arr[i] == key) {
-            cout << "Found at index " << i << endl;
-            found = true;
-            break;
+    for(int i=0; i<5; i++){
+        if(a[i] == key){
+            cout<<"Key is found at index: "<<i<<endl;
+            
         }
     }
-
-    if (!found)
-        cout << "Not found in array." << endl;
-
-    return 0;
 }
-
