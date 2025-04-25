be8141d27c7da340a6014acea3ec65c4c9446ffa
nitesh-20 <nsahu6156@gmail.com>
2025-04-25 20:26:06 +0530

    prime number

---
+++ Day128/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, i, isPrime = 1;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num <= 1) {
+        isPrime = 0;
+    } else {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        cout << num << " is a prime number." << endl;
+    else
+        cout << num << " is not a prime number." << endl;
+
+    return 0;
+}
