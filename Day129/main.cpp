99dd901d4ba844693207abfcb9c29893c3353acb
nitesh-20 <nsahu6156@gmail.com>
2025-04-26 20:00:51 +0530

    Fibonacci

---
+++ Day129/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a = 0, b = 1, next;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
