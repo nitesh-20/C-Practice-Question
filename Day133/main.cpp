64d88a19527db99df16725500e1b4bbe589293f4
nitesh-20 <nsahu6156@gmail.com>
2025-05-02 07:42:47 +0530

    Swap Two Numbers Without Third Variable

---
+++ Day133/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Before swap: a = " << a << ", b = " << b << endl;
+
+    // Swapping without temp
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    cout << "After swap: a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
