0596b0eee6514f6d676ccb02166526532c0c6e6e
nitesh-20 <nsahu6156@gmail.com>
2025-05-31 18:22:57 +0530

    Reversed a number

---
+++ Day150/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num, reversed = 0, digit;
+
+    cout << "Enter a Number: ";
+    cin >> num;
+
+    while(num != 0 ){
+        digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+    cout << "Reversed Number: " << reversed << endl;
+    return 0;
+}
