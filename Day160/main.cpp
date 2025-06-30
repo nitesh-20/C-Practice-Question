56ad5a424d46fdf1532c006333b1acca45f39f90
nitesh-20 <nsahu6156@gmail.com>
2025-06-30 22:30:08 +0530

    Find Minimum and Maximum in an Array

---
+++ Day160/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int min = arr[0], max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+    }
+
+    cout << "Min: " << min << ", Max: " << max << endl;
+    return 0;
+}
