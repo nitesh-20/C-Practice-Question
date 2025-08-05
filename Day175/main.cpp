bee38d5cf3ea3df7929aa70dcc83eccafcc3ad4b
nitesh-20 <nsahu6156@gmail.com>
2025-08-05 13:46:05 +0530

    Find the maximum sum of a contiguous subarray and also return the starting and ending index of that subarray.

---
+++ Day175/main.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+
+        int maxSum = INT_MIN, currSum = 0;
+        int start = 0, tempStart = 0, end = 0;
+
+        for(int i = 0; i < n; i++){
+            currSum += arr[i];
+
+            if(currSum > maxSum) {
+                maxSum = currSum;
+                start = tempStart;
+                end = i;
+            }
+
+            if(currSum < 0) {
+                currSum = 0;
+                tempStart = i + 1;
+            }
+        }
+
+        cout << "Maximum Subarray Sum: " << maxSum << endl;
+        cout << "Subarray Indices: [" << start << ", " << end << "]" << endl;
+
+        cout << "Subarray Elements: ";
+        for(int j = start; j <= end; j++) {
+            cout << arr[j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
