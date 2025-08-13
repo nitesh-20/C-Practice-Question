fdbb724cacfd3f15d650fc3c7cdacee06c733278
nitesh-20 <nsahu6156@gmail.com>
2025-08-13 13:07:57 +0530

    Breadth First Search (BFS)

---
+++ Day177/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+// Breadth First Search (BFS) for a graph using adjacency list
+void bfs(int start, const vector<vector<int>>& adj, int n) {
+    vector<bool> visited(n, false);
+    queue<int> q;
+    visited[start] = true;
+    q.push(start);
+    cout << "BFS Traversal: ";
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int n = 5; // Number of nodes
+    vector<vector<int>> adj(n);
+    // Example edges
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+
+    bfs(0, adj, n);
+    return 0;
+}
