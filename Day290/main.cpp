85ace380e408bb97e919c78e391722819ac5dcec
nitesh-20 <nsahu6156@gmail.com>
2026-05-29 22:16:34 +0530

    Day 290: Add Binary Search Tree program

--- Day290/main.cpp
+++ Day290/main.cpp
@@ -2,7 +2,49 @@
 
 using namespace std;
 
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+Node* insert(Node* root, int val) {
+    if (root == nullptr) {
+        return new Node(val);
+    }
+    if (val < root->data) {
+        root->left = insert(root->left, val);
+    } else {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root == nullptr) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
 int main() {
-    cout << "Day 290" << endl;
+    Node* root = nullptr;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    cout << "Day 290: Binary Search Tree Inorder Traversal" << endl;
+    inorder(root);
+    cout << endl;
     return 0;
 }
+
